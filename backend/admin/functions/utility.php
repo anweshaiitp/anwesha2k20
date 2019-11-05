745c866efab8225f02b99484d77c33981ed0d078
Amartya Mondal <hayyoulistentome@gmail.com>
2019-11-06 03:48:58 +0530

    feat: Fixed the user backend

--- backend/admin/functions/utility.php
+++ backend/admin/functions/utility.php
@@ -11,21 +11,21 @@ function send_email($email,$subject,$msg,$headers){
 	//$mail->SMTPDebug = 4;                               // Enable verbose debug output
 
 	$mail->isSMTP();                                      // Set mailer to use SMTP
-	$mail->Host = 'tls://mail.celesta.org.in';  // Specify main and backup SMTP servers
+	$mail->Host = 'smtp';  // Specify main and backup SMTP servers
 	$mail->SMTPAuth = true;                               // Enable SMTP authentication
 	// $mail->Username = "atmcelesta2k19@gmail.com";                 // SMTP username
 	// $mail->Password ="celesta@2k19";
 	// $mail->Username = "celesta2k19@gmail.com";                 // SMTP username
 	// $mail->Password = "celesta19@iitp.ac.in"; 
-	$mail->Username = "curator@celesta.org.in";                 // SMTP username
-	$mail->Password = "celesta19@iitp.ac.in"; 
+	$mail->Username = "email";                 // SMTP username
+	$mail->Password = "password"; 
 	$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 	$mail->Port = 587;                                    // TCP port to connect to
 
-	$mail->setFrom('curator@celesta.org.in', 'Celesta2k19');
+	$mail->setFrom('email', 'Anwesha2k20');
 	$mail->addAddress($email);     // Add a recipient
 	//$mail->addAddress('ellen@example.com');               // Name is optional
-	$mail->addReplyTo('curator@celesta.org.in', 'Information');
+	$mail->addReplyTo('email', 'Information');
 	//$mail->addCC('cc@example.com');
 	//$mail->addBCC('bcc@example.com');
 
@@ -88,26 +88,35 @@ function send_bulk_email($data_emails,$subject,$msg,$headers){
 		}
 	}
 
-//Function to generate random celestaID
-function getCelestaId(){
-	$exist=true;
-	while ($exist) {
-		$celestaid="CLST".mt_rand(1001,9999);
-		$exist=celestaid_exists($celestaid);
-	}
-	return $celestaid;
-}
+// //Function to generate random celestaID
+// function getCelestaId(){
+// 	$exist=true;
+// 	while ($exist) {
+// 		$celestaid="CLST".mt_rand(1001,9999);
+// 		$exist=celestaid_exists($celestaid);
+// 	}
+// 	return $celestaid;
+// }
 
-//To check if the given username already exists or not
-function celestaid_exists($celestaid){
-	$sql="SELECT id FROM users WHERE celestaid='$celestaid'";
+// //To check if the given username already exists or not
+// function celestaid_exists($celestaid){
+// 	$sql="SELECT id FROM users WHERE celestaid='$celestaid'";
+// 	$result=query($sql);
+// 	if(row_count($result)==1){
+// 		return true;
+// 	}
+// 	else{
+// 		return false;
+// 	}
+// }
+
+function getAnweshaId(){
+	$sql="SELECT anweshaid FROM users ORDER BY id DESC LIMIT 1";
 	$result=query($sql);
-	if(row_count($result)==1){
-		return true;
-	}
-	else{
-		return false;
+	if($result==1){
+		$row=fetch_array($result);
+		return "ANW".substr($row['anweshaid'],3,7);
+	}else{
+		return "ANW2000";
 	}
 }
-
-
