f35e016177ac2b36fdda648dde4b0aafdb83ce7e
Subhang23 <vsubhang4@gmail.com>
2019-11-07 00:44:07 +0530

    changes for sign in

--- backend/user/functions/utility.php
+++ backend/user/functions/utility.php
@@ -12,7 +12,7 @@ function send_email($email,$subject,$msg,$headers){
 	$mail->isSMTP();                                      // Set mailer to use SMTP
 	$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 	$mail->SMTPAuth = true;                               // Enable SMTP authentication
-	$mail->Username = "pranaykgupta21@gmail.com";                 // SMTP username
+	$mail->Username = "vsubhang4@gmail.com";                 // SMTP username
 	$mail->Password = "password"; 
 	$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 	$mail->Port = 587;                                    // TCP port to connect to
